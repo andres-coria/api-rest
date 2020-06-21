0edace9f7d6659c14332593e96df0ffc97758d6e
Andres Coria <anco22@yahoo.com>
2020-06-21 20:31:57 -0300

    typescript src

--- src/models/user.ts
+++ src/models/user.ts
@@ -14,7 +14,6 @@ const userSchema = new Schema({
     unique: true,
     required: true,
     trim: true,
-    // Regexp to validate emails with more strict rules as added in tests/users.js which also conforms mostly with RFC2822 guide lines
     match: [/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Please enter a valid email']
   },
   password: {
